d5abe4b1531d5147d93391755478f721ede96e75
Endriw de Oliveira <endriwdeoliveirafarias96@gmail.com>
2023-04-24 16:06:36 -0300

    criado o webapp de exemplo com o flask

---
+++ primeiro_webapp.py
@@ -0,0 +1,15 @@
+#Importando o flask para poder usar no nosso progama
+from flask import Flask
+
+#Vamos criar uma variável(neste caso, um objeto) para representar nossa aplicação web
+app = Flask(__name__)
+
+#Vamos criar uma rota para acessar o servidor
+@app.route("/")
+#Ao acessar essa rota a função abaixo é executada e ela devolve (return) o texto "Deu certo!"
+def exibir_mensagem():
+    print("Onde aparece?")
+    return "Deu certo!"
+
+#A linha abaixo executa a aplicação web que foi criada a paritir do flask
+app.run(debug=True)
